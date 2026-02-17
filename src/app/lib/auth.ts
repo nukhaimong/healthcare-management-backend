495bfd8e2bd86ac07d7455bf6ad2b28d966b1a57
nukhaimong <nukhaimongmarma834@gmail.com>
2026-02-18 00:55:42 +0600

    add some new models

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -2,8 +2,6 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { prisma } from './prisma';
 import { Role, UserStatus } from '../../generated/prisma/enums';
-import ms from 'ms';
-import { envVars } from '../../config/env';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
